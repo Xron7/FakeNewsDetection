5ea5917a12b8803b01206bd669f504cc0cdced59
Panagiotis Chronopoulos <panoschrono@gmail.com>
2024-12-14 15:52:27 +0200

    minor removal

--- models/random_forest.py
+++ models/random_forest.py
@@ -2,7 +2,7 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print("Current Working Directory:", os.getcwd())
+
 import pandas as pd
 
 from sklearn.ensemble                import RandomForestClassifier
