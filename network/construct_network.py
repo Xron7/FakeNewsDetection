faac534e59e8adf91bc6c8b9575ab932c791fceb
Xron7 <panoschrono@gmail.com>
2025-01-17 18:09:16 +0200

    network analysis init

--- network/construct_network.py
+++ network/construct_network.py
@@ -1,23 +1,24 @@
 import os
+import networkx as nx
 
 from tqdm import tqdm
 
 from config import PATH
-from utils  import construct_prop_df
 
 data_path = PATH + 'tree/'
 
+sources = []
+targets = []
+weights = []
+
 for file_name in tqdm(os.listdir(data_path), desc= 'Constructing Network'):
-    with open(data_path + '752965545528528898.txt', 'r') as file:
+    with open(data_path + file_name, 'r') as file:
 
         # first line for the poster
-        first_line   = file.readline().strip()
-        root, poster = first_line.split('->')
+        first_line = file.readline().strip()
+        root, _    = first_line.split('->')
 
-        root   = eval(root)
-        poster = eval(poster)
-
-        poster = poster[0]
+        root = eval(root)
 
         # check if there is time error
         time_shift = 0
@@ -25,20 +26,19 @@ for file_name in tqdm(os.listdir(data_path), desc= 'Constructing Network'):
             print(f'Detected time issue for {file_name}')
             time_shift = -1 * float(root[2])
 
-        # separate handling to remove the ROOT
-        if time_shift:
-            pass
-        else:
-            pass
-        # for i, line in enumerate(file):
-        #     source, retweet = line.split('->')
-        #     source  = eval(source)
-        #     retweet = eval(retweet)
-        #     print(source, retweet)
-        #
-        #     poster = retweet[0]
-        #
-        #     break
+        for i, line in enumerate(file):
+            source, retweet = line.split('->')
+            source  = eval(source)
+            retweet = eval(retweet)
 
-    break
+            if source[0]!='ROOT':
+                sources.append(source[0])
+                targets.append(retweet[0])
+                weights.append(round(float(retweet[2]) - float(source[2]) + time_shift, 2))
 
+G = nx.DiGraph()
+
+edges = zip(sources, targets, weights)
+G.add_edges_from((source, retweeter, {'weight': weight}) for source, retweeter, weight in edges)
+
+nx.write_weighted_edgelist(G, "network.csv", delimiter=',')
