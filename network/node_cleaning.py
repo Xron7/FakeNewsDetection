df2a8941404344053ed6959df313f7553356d442
Panagiotis Chronopoulos <panoschrono@gmail.com>
2025-05-21 21:14:38 +0300

    more work needed

--- network/node_cleaning.py
+++ network/node_cleaning.py
@@ -7,24 +7,26 @@ from config import PATH
 # Read Inputs
 
 G = nx.DiGraph()
-G = nx.read_edgelist(PATH + "network.csv", delimiter=",", nodetype=int, data=(('weight', float),), create_using=nx.DiGraph())
+G = nx.read_edgelist(PATH + "network.csv",
+                     delimiter=",", nodetype=int, data=(('weight', float),), create_using=nx.DiGraph())
 
 with open(sys.argv[1], "r") as f:
     nodes_to_remove = [int(line.strip()) for line in f if line.strip()]
 
+########################################################################################################################
+# Remove nodes and the nodes that depend on them
+
 num_nodes_og = G.number_of_nodes()
 G.remove_nodes_from(nodes_to_remove)
-print(f'{len(nodes_to_remove)}/{num_nodes_og} nodes removed')
 
-    # while True:
-    #     # Find all nodes with no incoming edges
-    #     no_incoming = [n for n in G.nodes if G.in_degree(n) == 0]
+total_nodes_removed = len(nodes_to_remove)
+while True:
+    no_incoming = [n for n in G.nodes if G.in_degree(n) == 0]
+    total_nodes_removed += len(no_incoming)
 
-    #     # If no such nodes remain, we're done
-    #     if not no_incoming:
-    #         break
+    if not no_incoming:
+        break
 
-    #     # Remove them
-    #     G.remove_nodes_from(no_incoming)
+    G.remove_nodes_from(no_incoming)
 
-    # return G
+print(f'{total_nodes_removed}/{num_nodes_og} nodes removed')
